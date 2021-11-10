0563bf429409eae99d6fd2c21d11eb4079d54770
bietdoikiem <minhlucky2408@gmail.com>
2021-11-11 01:16:40 +0700

    refactor: separate data structures & implementations

---
+++ ArrayQueue.java
@@ -0,0 +1,120 @@
+
+public class ArrayQueue<T> {
+  private int capacity;
+  private int size;
+  private T[] array;
+
+  public ArrayQueue(int capacity) {
+    this.capacity = capacity;
+    this.size = 0;
+    this.array = (T[]) new Object[capacity];
+  }
+
+  /**
+   * Get the current size of the queue
+   * 
+   * @return
+   */
+  public int getSize() {
+    return this.size;
+  }
+
+  /**
+   * Check if the queue is currently empty
+   * 
+   * @return True if empty | False if not
+   */
+  public boolean isEmpty() {
+    return size == 0;
+  }
+
+  /**
+   * Check if the queue is full
+   * 
+   * @return True if empty | False if not
+   */
+  public boolean isFull() {
+    return size == capacity;
+  }
+
+  /**
+   * Enqueue an item to the queue | Time complexity: O(1)
+   * 
+   * @param item
+   * @return
+   */
+  public T enqueue(T item) {
+    // CASE 1: If queue is full
+    if (isFull()) {
+      System.out.println("ERROR! The queue is currently full!");
+      return null;
+    }
+    // CASE 2: If queue is still insert-able
+    size++;
+    array[size - 1] = item;
+    return item;
+  }
+
+  /**
+   * Dequeue an item from the queue | Time complexity: O(n)
+   * 
+   * @return Dequeued item
+   */
+  public T dequeue() {
+    if (isEmpty()) {
+      System.out.println("ERROR! The queue is currently empty");
+      return null;
+    }
+    T poppedItem = array[0];
+    // Shift all items to left
+    for (int i = 0; i < size - 1; i++) {
+      array[i] = array[i + 1];
+    }
+    // Set null to last dangling item in queue
+    array[size - 1] = null;
+    size--;
+    return poppedItem;
+  }
+
+  /**
+   * Peek at the first item in the queue | Time complexity: O(1)
+   * 
+   * @return Item
+   */
+  public T peekFront() {
+    if (isEmpty()) {
+      System.out.println("The queue is currently empty");
+      return null;
+    }
+    return array[0];
+  }
+
+  /**
+   * Peek at the last item of the queue | Time complexity: O(1)
+   * 
+   * @return
+   */
+  public T peekRear() {
+    if (isEmpty()) {
+      System.out.println("The queue is currently empty");
+      return null;
+    }
+    return array[size - 1];
+  }
+
+  /**
+   * Display all the items currently in the queue | Time complexity: O(n)
+   */
+  public void display() {
+    if (isEmpty()) {
+      System.out.println("The queue is currently empty");
+      return;
+    }
+    System.err.print("The queue: ");
+    for (int i = 0; i < size; i++) {
+      System.out.print(array[i] + " ");
+    }
+    System.out.print("\n");
+  }
+
+}
---
+++ LinkedList.java
@@ -0,0 +1,123 @@
+public class LinkedList {
+  public Node head;
+
+  static class Node {
+    public int value;
+    public Node next;
+
+    public Node(int value) {
+      this.value = value;
+    }
+  }
+
+  /**
+   * Insert/Append a new value to
+   * 
+   * @param value
+   * @return
+   */
+  public Node insert(int value) {
+
+    // If linked list is empty
+    if (head == null) {
+      Node insertedNode = new Node(value);
+      this.head = insertedNode;
+      head.next = null;
+      return insertedNode;
+    } else {
+      // Traverse to the end of node
+      Node insertedNode = new Node(value);
+      Node cur = this.head;
+      while (cur.next != null) {
+        cur = cur.next;
+      }
+      cur.next = insertedNode;
+      return insertedNode;
+    }
+  }
+
+  /**
+   * Delete node of linked list by key/value
+   * 
+   * @param key
+   * @return
+   */
+  public Node deleteAtKey(int key) {
+    // Init pointers
+    Node cur = this.head;
+    Node prev = null;
+
+    // CASE 1: Delete at head
+    if (cur != null && cur.value == key) {
+      this.head = cur.next;
+      System.out.println(key + " was found and deleted");
+      return cur;
+    }
+
+    // Traverse until key is matched or not null
+    while (cur != null && cur.value != key) {
+      prev = cur;
+      cur = cur.next;
+    }
+
+    // CASE 2: Delete at middle or above
+    if (cur != null) {
+      prev.next = cur.next;
+      System.out.println(key + " was found and deleted");
+      return cur;
+    }
+
+    System.out.println(key + " was not found");
+    return null;
+
+  }
+
+  public Node deleteByPos(int pos) {
+    /**
+     * Success Flow: CASE 1: Delete at Head CASE 2: Delete at Middle CASE 3: Pos
+     * exceeded/Not-found CASE 3: Not found
+     */
+    // 2 pointers
+    Node prev = null;
+    Node cur = this.head;
+
+    // CASE 1: DELETE AT Head
+    if (cur != null && pos == 0) {
+      this.head = cur.next;
+      System.out.println("Node at position " + pos + " deleted");
+      return cur;
+    }
+
+    int idxCounter = 0;
+    // Traverse from the beginning
+    while (cur != null && idxCounter != pos) {
+      prev = cur;
+      cur = cur.next;
+      idxCounter++;
+    }
+
+    // CASE 2: Delete at Middle
+    if (cur != null) {
+      prev.next = cur.next;
+      System.out.println("Node at position " + pos + " deleted");
+      return cur;
+    }
+
+    System.out.println("Cannot delete node at position " + pos);
+
+    return null;
+  }
+
+  /**
+   * Print list of nodes' values in the linked list
+   */
+  public void printList() {
+    Node cur = this.head;
+    System.out.print("Linked list: ");
+
+    while (cur != null) {
+      System.out.print(cur.value + " ");
+      cur = cur.next;
+    }
+  }
+}
